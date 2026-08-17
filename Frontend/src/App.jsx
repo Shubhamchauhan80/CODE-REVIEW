6e2eb432331e29a0ab196fe031e7ffc176620c71
Shubham Kumar <sk1314518@gmail.com>
2026-08-17 18:29:59 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   // }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://code-review-backend-ent6.onrender.com', { code })
     setReview(response.data)
   }
 
